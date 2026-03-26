5dc715d9b64ba417728fbc1582bc20890108429e
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2026-03-26 04:04:43 -0700

    Beta Connect build: 1.0.113
    Beta Connect Change Notes:
    Bug Fixes:
    
    - Default to system value for User-Agent header if it is not available
    
    Improvements:
    
    - Updated library using latest Xcode and iOS versions (26.2)
    
    Our environment for this release:
    
    - Xcode 26.2
    - MacOS 26.0
    - iOS 15.1 to 26.2
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues:
    
    - Captures gesture even when the gestureRecognizer is set to return 'NO', this is an Apple defect.
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - The pink boarder on Gestures screenview is off when captured using ER, because if timing or garbage collection.
    - Template issue - The back link is missing during capture on some screens Progressindicator, AlertViewController, Gestures screenview and picker view.
    - Template issue - Battery icon not visible in green anymore.
    - Improvement :- 'connectionType' remains N/A if device is on WiFi on app start.
    - DoPostOnScreenChange is not working as expected, because of temp queue.
    - Reporting of multiple carrier names in JSON if the device has Dual SIM is a beta feature.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Certain Alerts may appear correctly during replay.
    - If a View Controller has more than one WKWebViews, gestures may be off by a few pixels
    - iPadOS Multi Window apps are not YET supported.

--- Package.swift
+++ Package.swift
@@ -34,8 +34,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Connect",
-            url: "https://github.com/go-acoustic/Connect/releases/download/1.0.112/Connect_XCFramework_Debug.zip",
-            checksum: "de211dec10eb1434156286b84cb27260e514f3ab55f9c1f60888c7f85bde5059"),
+            url: "https://github.com/go-acoustic/Connect/releases/download/1.0.113/Connect_XCFramework_Debug.zip",
+            checksum: "08f0e26649d7b97e0a80c495c412ef51ab22652e0393b064737b16607761bc54"),
         .binaryTarget(
             name: "Tealeaf",
             url: "https://github.com/go-acoustic/Tealeaf/releases/download/10.6.335/Tealeaf_XCFramework_Debug.zip",
